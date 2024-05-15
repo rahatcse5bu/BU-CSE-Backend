a07f0b70b52ea1f35c42a66db1730589b31b910d
unknown <anis.cse5.bu@gmail.com>
2024-05-15 14:11:21 +0600

    added

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
+const cors = require("cors");
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
